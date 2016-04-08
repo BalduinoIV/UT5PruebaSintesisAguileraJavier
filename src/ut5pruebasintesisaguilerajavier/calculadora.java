791c8ff15203851d7b777cdf8f687674e089a426
1-DAM-T <1-DAM-T>
2016-04-08 15:35:50 +0100

    paso1y2

---
+++ src/ut5pruebasintesisaguilerajavier/calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut5pruebasintesisaguilerajavier;
+
+/**
+ *
+ * @author 1-DAM-T
+ */
+public class calculadora {
+    private String operador1;
+    private String operador2;
+    private String operacion;
+}
